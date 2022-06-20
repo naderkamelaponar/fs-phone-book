168603169ce41f799c009854aa320f3f47d7eacd
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-20 14:33:40 +0200

    feat: save the phone book in a db

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const handleSubmit=(event)=>{
 return (
 <>
 <h1> بسم الله الرحمن الرحيم</h1>
-<h2>Phone Book 2.10</h2>
+<h2>Phone Book 2.11</h2>
 <h3>Filter :</h3>
 <div> Filter : <input placeholder="Filter the names" value={fValue}  onChange={handleFilter}/></div>
 <AddNew name={name} number={number} handleName={handleName} handleNumber={handleNumber} handleSubmit={handleSubmit}/>
