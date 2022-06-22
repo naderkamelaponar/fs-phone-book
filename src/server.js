4eab41333a34fa732d44e2df4510f6656a241414
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-22 16:42:50 +0200

    setup server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 const jsonServer = require('./json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+server.use(router)
 const port=3001;
 server.listen(port, () => {
     console.log(`Tdb server is up`)
